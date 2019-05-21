c40543d3b3e378ff86e4c1ad25a3d4d6eb7e7d18
Gustavo Marques <gustavo@trackmob.com.br>
2019-05-21 15:02:02 -0300

    show selected video

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import VideoListItem from './VideoListItem';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map(video => {
-    return <VideoListItem video={video} />;
+    return <VideoListItem onVideoSelect={onVideoSelect} video={video} />;
   });
 
-  return <div>{renderedList}</div>;
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
--- src/components/VideoListItem.js
+++ src/components/VideoListItem.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './VideoListItem.css';
 
-const VideoListItem = ({ video }) => {
+const VideoListItem = ({ video, onVideoSelect }) => {
   return (
-    <div>
-      <img src={video.snippet.thumbnails.medium.url} alt="" />
-      {video.snippet.title}
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
+      <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt={video.snippet.title}
+      />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
     </div>
   );
 };
